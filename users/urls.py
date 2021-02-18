89418fa0407a393cd097d3a51a53f09ad3952a3f
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-02-18 02:20:11 +0000

    create login page and view function based on LoginView class

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views as v
 
 urlpatterns = [
-    path('registration/', v.registration, name='user-registration')
+    path('registration/', v.registration, name='user-registration'),
+    path('login/', v.UserLoginView.as_view(), name='user-login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from . import forms as f
 from django.contrib import messages
+from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.models import User
 
 
 def registration(request):
@@ -17,3 +19,14 @@ def registration(request):
         form = f.UserRegistrationForm()
 
     return render(request, 'users/registration.html', context={'form': form})
+
+
+class UserLoginView(LoginView):
+
+    class Meta:
+        # model = User
+        template_name = 'users/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
