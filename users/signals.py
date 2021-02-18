cd78ac1cd2727f726da143d6514a42487e00e0b0
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-02-18 12:12:14 +0000

    directly import Profile model

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,5 @@
 from django.db.models.signals import post_save
-from . import models as m
+from .models import Profile
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 @receiver(signal=post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        m.Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
 
 
 @receiver(signal=post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    m.Profile.save(user=instance)
+    instance.profile.save()
 
 
 
